b243c9fdfff17bf3f1f22f250a15cf5eaaeead77
Harigai <108782529+Jothiharigai-A@users.noreply.github.com>
2024-07-18 10:52:58 +0530

    initial
    commit

---
+++ src/electricity/billing/system/database.java
@@ -0,0 +1,14 @@
+package electricity.billing.system;
+import java.sql.*;
+public class database {
+    Connection connection;
+    Statement statement;
+    database(){
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Bill_system","root","Kaarigai@12$");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
